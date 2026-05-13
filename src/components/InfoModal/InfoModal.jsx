ce2d89ae8998e643468c18c939911c466c1d5b5d
EM-H20 <ghd0106@gmail.com>
2026-05-13 18:40:53 +0900

    fix: InfoModal 두 번째 문단 줄바꿈 추가 #39

--- src/components/InfoModal/InfoModal.jsx
+++ src/components/InfoModal/InfoModal.jsx
@@ -18,7 +18,7 @@ export default function InfoModal({ onClose }) {
           <p className="info-modal__horror">그린이는 나야,</p>
           <p className="info-modal__horror">둘이 될 수 없어.</p>
           <p>그린이의 얼굴을 한 &ldquo;가짜&rdquo;가 그린이를 헤치려 합니다.</p>
-          <p>그린이 행세를 하는 &ldquo;가짜&rdquo;를 물리치기 위해 4가지 게임을 통과해야합니다.</p>
+          <p>그린이 행세를 하는 &ldquo;가짜&rdquo;를 물리치기 위해<br />4가지 게임을 통과해야합니다.</p>
         </div>
       </div>
     </div>
